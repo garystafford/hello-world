a68c53a1d16d79daf110db288b8f203a0c6c5d87
Gary A. Stafford <garystafford@rochester.rr.com>
2017-09-21 14:09:16 -0400

    Add error endpoint

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ unset DOCKER_TLS_VERIFY DOCKER_CERT_PATH DOCKER_HOST
 # build
 ./gradlew clean build
 
-
 source ~/Documents/Notes/dockerhub_login.env
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 docker build --no-cache -t garystafford/hello-logging:latest .
