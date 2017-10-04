ac164ac043bf715be0d0bed906ce203cb8f7f8f7
Gary A. Stafford <garystafford@rochester.rr.com>
2017-10-04 00:34:13 -0400

    Adding auto docker hub build

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ unset DOCKER_TLS_VERIFY DOCKER_CERT_PATH DOCKER_HOST
 
 source ~/Documents/Notes/dockerhub_login.env
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
-docker build --no-cache -t garystafford/hello-logging:latest .
-docker push garystafford/hello-logging:latest
+docker build --no-cache -t garystafford/hello-world:latest .
+docker push garystafford/hello-world:latest
